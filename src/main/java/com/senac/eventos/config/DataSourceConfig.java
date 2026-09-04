927c22caf2204fb733c072b6d6523a372e4738b5
Nate-Allen21 <ns3027192@gmail.com>
2026-09-03 21:38:56 -0300

    Corrige parsing da URI do Supabase

--- src/main/java/com/senac/eventos/config/DataSourceConfig.java
+++ src/main/java/com/senac/eventos/config/DataSourceConfig.java
@@ -1,5 +1,7 @@
 package com.senac.eventos.config;
 
+import java.net.URI;
+
 import javax.sql.DataSource;
 
 import org.springframework.boot.autoconfigure.jdbc.DataSourceProperties;
@@ -15,7 +17,27 @@ public class DataSourceConfig {
     DataSource dataSource(DataSourceProperties properties) {
         String url = properties.getUrl();
         if (url != null && url.startsWith("postgresql://")) {
-            properties.setUrl("jdbc:" + url);
+            URI databaseUri = URI.create(url);
+            String userInfo = databaseUri.getUserInfo();
+            String jdbcUrl = "jdbc:postgresql://" + databaseUri.getHost();
+            if (databaseUri.getPort() != -1) {
+                jdbcUrl += ":" + databaseUri.getPort();
+            }
+            if (databaseUri.getRawPath() != null) {
+                jdbcUrl += databaseUri.getRawPath();
+            }
+            if (databaseUri.getRawQuery() != null) {
+                jdbcUrl += "?" + databaseUri.getRawQuery();
+            }
+
+            properties.setUrl(jdbcUrl);
+            if (userInfo != null) {
+                String[] credentials = userInfo.split(":", 2);
+                properties.setUsername(credentials[0]);
+                if (credentials.length == 2) {
+                    properties.setPassword(credentials[1]);
+                }
+            }
         }
         return properties.initializeDataSourceBuilder().build();
     }
